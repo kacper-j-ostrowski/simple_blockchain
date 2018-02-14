c09b7ce1b8deb8809e86d37336eaded946e14c50
kacper_ostr <kacperjostrowski@gmail.com>
2018-02-14 21:46:36 +0100

    Initial commit

---
+++ src/pl/ostrowski/blockchain/SimpleBlockchainMain.java
@@ -0,0 +1,56 @@
+package pl.ostrowski.blockchain;
+
+import com.google.gson.GsonBuilder;
+import pl.ostrowski.blockchain.block.Block;
+
+import java.util.ArrayList;
+
+public class SimpleBlockchainMain {
+
+
+    public static ArrayList<Block> blockchain = new ArrayList<>();
+    public static int difficulty = 5;
+
+    public static void main(String[] args){
+
+
+        blockchain.add(new Block("Hi im the first block", "0"));
+        System.out.println("Trying to Mine block 1... ");
+        blockchain.get(0).mineBlock(difficulty);
+
+        blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 2... ");
+        blockchain.get(1).mineBlock(difficulty);
+
+        blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 3... ");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockchain is Valid: " + isChainValid());
+
+        String blockChainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+
+        System.out.println(blockChainJson);
+    }
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        for(int i=1; i<blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+
+            if(!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current hashes not equal");
+                return false;
+            }
+
+            if(!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous hash not equal");
+                return false;
+            }
+        }
+        return true;
+    }
+}
